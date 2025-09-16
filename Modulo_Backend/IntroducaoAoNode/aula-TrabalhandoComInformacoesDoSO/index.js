b30f0962e64d92119c68bc880100442aa5d694c8
Gustavo Alves <gustavodevgarcia@gmail.com>
2025-09-16 07:54:36 -0300

    feat: adiciona uma nova aula do curso 'trabalhando com o so'

---
+++ Modulo_Backend/IntroducaoAoNode/aula-TrabalhandoComInformacoesDoSO/index.js
@@ -0,0 +1,16 @@
+const os = require("node:os")
+
+const plataforma = os.platform()
+console.log("Plataforma do SO: ", plataforma)
+
+const versao = os.version()
+console.log("Versão do SO: ", versao)
+
+const arquitetura = os.arch()
+console.log("Arquitetura do SO: ", arquitetura)
+
+const processadores = os.cpus()
+console.log("Informações da CPU:", processadores[0])
+
+const memoria = os.totalmem()
+console.log("Total de memória do PC:", memoria / 1024 / 1024 / 1024, "GB")
