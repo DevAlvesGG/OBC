333964e59cbea9f5ae609dadef3b7c5c8c20c72c
Gustavo Alves <gustavodevgarcia@gmail.com>
2025-11-26 09:35:03 -0300

    feat: aprendendo requisicoes post e redirecionamentos

--- Modulo_Backend/AplicacoesWebComNode/aula-RenderizandoHTMLDinamico/src/server.js
+++ Modulo_Backend/AplicacoesWebComNode/aula-RenderizandoHTMLDinamico/src/server.js
@@ -3,16 +3,40 @@ const path = require('node:path');
 
 const app = express()
 
+//configuração do EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//configuração do body
+app.use(express.urlencoded({ extended: true }))
+
 const PORT = 3000;
 
-app.get('/', (req,resp) => {
+const storedUsers = [];
+
+app.get('/', (req,res) => {
     const title = "Título dinâmico passado pelo servidor"
     const text = "Esse é um texto dinâmico passado pelo servidor para a página HTML"
 
-    resp.render('index', { title, text })
+    res.render('index', { title, text })
 })
+
+app.get('/formulario', (req, resp) => {
+    resp.render('form')
+})
+
+app.get('/usuarios', (req,res) => {
+    res.render('users', { users: storedUsers})
+})
+
+app.post('/register', (req,res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    storedUsers.push({ username, password })
+
+    res.redirect('/usuarios') 
+})
+
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
